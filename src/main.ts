7e7d7004ab1786bdc961a7583aa28c1e968ce3de
rancwb <henriqsantos629@gmail.com>
2025-04-03 12:45:03 -0300

    a

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
   app.enableCors({
     origin: ['https://pdf-front-six.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, 
   });
   
+
+  await app.listen(process.env.PORT ?? 3000);
 
 }
 bootstrap();
