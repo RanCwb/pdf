eb0f891ea0f39b292900b879363703214c8b5a81
rancwb <henriqsantos629@gmail.com>
2025-04-03 12:54:16 -0300

    fix cors

--- src/cors.middleware.ts
+++ src/cors.middleware.ts
@@ -4,7 +4,7 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
     next();
