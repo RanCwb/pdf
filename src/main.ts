41a1d21e63aa891cd502d94ca9cc249e1688c6a0
rancwb <henriqsantos629@gmail.com>
2025-04-03 12:35:45 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://pdf-front-six.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, 
+  });
+  
 
 }
 bootstrap();
